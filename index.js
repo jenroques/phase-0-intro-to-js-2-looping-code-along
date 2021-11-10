e0d54bc4983c9a85ada69c8917d6b39dc0c8cb61
Jennifer <jenniferroques@gmail.com>
2021-11-10 14:52:40 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 //for (let age = 30; age < 40; age++) {
-  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
-  //  debugger;
- // }
+//  console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
 
-  //const gifts = ["teddy bear", "drone", "doll"];
+///const gifts = ["teddy bear", "drone", "doll"];
 
 //function wrapGifts(gifts) {
   //for (let i = 0; i < gifts.length; i++) {
     //console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    //debugger;
-  //}
-
+    
+ // }
   //return gifts;
 //}
 
-//wrapGifts(gifts);
+//wrapGifts(gifts)
 
-const thankYous = ["Penny", "Matt", "Ding"]; 
-
-function writeCards(thankYous) {
-    for (let i = 0; i < thankYous.length; i++) {
-        console.log(`Thank you, ${thankYous[i]} for the well wishes!`)
+function writeCards(name, event) {
+  let thankYous =[]
+    for (let i = 0; i < name.length; i++) {
+        console.log(thankYous.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`))
        }
        return thankYous;
 }
 
-writeCards(thankYous)
+writeCards();
 
-let countDown = 10; 
-while (countDown > -1) {
-    console.log(countDown--)
+
+
+function countDown(dec) {
+  let i=10
+  while (i >= 0) {
+    console.log(i);
+    i--
+ }
+  return dec
 }
